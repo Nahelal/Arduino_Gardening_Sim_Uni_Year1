83d1591c204d9ae4f6310608d53960f8290ec428
LW260328 <lw260328@falmouth.ac.uk>
2022-04-25 13:27:13 +0100

    still on fork?

--- Unity Project/Assets/Scripts/PlayerLives.cs
+++ Unity Project/Assets/Scripts/PlayerLives.cs
@@ -8,7 +8,6 @@ public class PlayerLives : MonoBehaviour
     //array of player lives
     public GameObject[] heartModel;
     private int lives = 3;
-    private bool noLives;
 
     // Start is called before the first frame update
     void Start()
@@ -19,26 +18,22 @@ public class PlayerLives : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        if (noLives = true)
-        {
-            //if player loses all 3 lives, they go back to the menu screen
-            // SceneManager.LoadScene("Menu");
-            Debug.Log("game overrrr :D ");
-        }
-    }
+    { }
+
 
     public void wrongInput()
     {
         if (lives >= 1)
         {
-            //
+            //heart is removed from UI and player life -1
             lives -= 1;
             Destroy(heartModel[lives].gameObject);
 
-            if (lives < 1)
+            if (lives == 0)
             {
-                noLives = true;
+                //if player loses all 3 lives, they go back to the menu screen
+                Debug.Log("game overrrr :D ");
+                SceneManager.LoadScene("Menu");
             }
         }
     }
--- Unity Project/Assets/Scripts/UduinoController.cs
+++ Unity Project/Assets/Scripts/UduinoController.cs
@@ -1,68 +1,124 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.IO.Ports;
+using Random = UnityEngine.Random;
 using Uduino;
 using UnityEngine.UI;
 
 
+
 public class UduinoController : MonoBehaviour
 {
+    //data inputs from arduino
+    public float buttonInp;
+    public float waterInp;
+    public float lightInp;
+    //public string DataReceived
 
-    public bool light = false;
-    public bool water = false;
-    public bool button = false;
+    int playerInp = 0;
+    int nextInp;
 
-    UduinoManager manager;
+    //UduinoManager manager;
 
     public GameObject myObject;
+
+  
 
     // Start is called before the first frame update
     void Start()
     {
-        manager = UduinoManager.Instance;
-        manager.pinMode(AnalogPin.A0, PinMode.Input);
+        //recieves all 3 uduino inputs
+        UduinoManager.Instance.OnDataReceived += DataReceived;
 
-        //gets inputs from the arduino and sends them to uduino to be used
-        UduinoManager.Instance.pinMode(6, PinMode.Input_pullup);
-        UduinoManager.Instance.pinMode(7, PinMode.Input_pullup);
     }
+
+    void DataReceived(string data, UduinoDevice board)
+    {
+
+        //recieve input data from arduino and splits into individual values to be read
+        string[] dataSplit = data.Split(',');
+        buttonInp = (float.Parse(dataSplit[0]));
+        waterInp = (float.Parse(dataSplit[1]));
+        lightInp = (float.Parse(dataSplit[2]));
+    }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        //button
-        int buttonInput = UduinoManager.Instance.digitalRead(6);
+       // int nextInp = Random.Range(1, 4);
 
-        if (buttonInput == 0)
+        //if (nextInp == 1)
+        //{
+       //     GameObject BottleBlack;
+       //     BottleBlack.SetActive(true);
+      //  }
+      //  else if (nextInp == 2)
+      //  {
+      //      GameObject SunBlack;
+      //      SunBlack.SetActive(true);
+      //  }
+      //  else if (nextInp == 3)
+      //  {
+     //       GameObject WateringCanBlack;
+     //       WateringCanBlack.SetActive(true);
+     //   }
+
+
+        // creates a number between 1 and 3
+
+        //assigns player input and checks if it matches requested input
+        while (playerInp == 0)
         {
-            myObject.GetComponent<PlayerLives>().wrongInput();
+            if (buttonInp == 1.0f)
+            {
+                playerInp = 1;
+            }
+
+            else if (waterInp >= 350.0f)
+            {
+                playerInp = 2;
+            }
+            else if (lightInp >= 120.0f)
+            {
+                playerInp = 3;
+            }
+            else //if player hasnt chosen input yet
+            {
+                playerInp = 0;
+            }
         }
+
+
+        if (playerInp == nextInp)
+        {
+            //choice is reset to zero for next iteration of while loop, and +1 is added to player score
+            playerInp = 0;
+
+
+         
+            myObject.GetComponent<Score>();
+            Score.totalScore += 1;
+
+        }
+
         else
-        {
-            myObject.GetComponent<FlowerState>().ChangePlantStage();
-        }
 
-        //light
-        int analogueValue = manager.analogRead(AnalogPin.A0);
-        if (analogueValue < 100)
         {
+            //player loses life if input is wrong 
             myObject.GetComponent<PlayerLives>().wrongInput();
-        }
-        else if (analogueValue > 100)
-        {
-            myObject.GetComponent<FlowerState>().ChangePlantStage();
-        }
-
-        //water
-        int waterLevelInput = UduinoManager.Instance.digitalRead(7);
-
-        if (waterLevelInput < 300)
-        {
-            myObject.GetComponent<PlayerLives>().wrongInput();
-        }
-        else
-        {
-            myObject.GetComponent<FlowerState>().ChangePlantStage();
         }
     }
 }
+  
+
+   
+
+
+
+
+
+
